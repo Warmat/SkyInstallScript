7bd5b94cf6ed4a024513413a0212a0c2e6487ac3
Warmat <31658555+Warmat@users.noreply.github.com>
2018-03-22 17:12:04 +0100

    Update ServiceStartSkywireSecondary.sh

--- ServiceStartSkywireSecondary.sh
+++ ServiceStartSkywireSecondary.sh
@@ -19,5 +19,5 @@
 cd $GOPATH/bin
 ./manager -web-dir ${GOPATH}/src/github.com/skycoin/skywire/static/skywire-manager > /dev/null 2>&1 &
 
-##### This starts the Skywire Monitor from the Master/Primary board (OrangePI 1)
+##### This starts the Node on your secondary board and shows it in the Manager of the Master/Primary board (OrangePI 1)
 ./node -connect-manager -manager-address 192.168.0.114:5998 -manager-web 192.168.0.101:8000  -address :5000 -web-port :6001 > /dev/null 2>&1 &
