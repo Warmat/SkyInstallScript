8be000b23a724b45826d214486bb554bb2441885
Warmat <31658555+Warmat@users.noreply.github.com>
2018-03-27 22:26:32 +0200

    Update SkyInstallScriptSecondary.sh

--- SkyInstallScriptSecondary.sh
+++ SkyInstallScriptSecondary.sh
@@ -132,7 +132,7 @@ sleep 5
 cd $GOPATH/bin
 ###### Starts Skywire Monitor service for the first time / after that command web interface of Monitor works
 ###### This line is to start the monitor on the slave nodes (OrangePI 2-8) only / Master Orange PIs requiere a different line!!!
-./node -connect-manager -manager-address 192.168.0.111:5998 -manager-web 192.168.0.111:8000  -address :5000 -web-port :6001 > /dev/null 2>&1 &
+./node -connect-manager -manager-address 192.168.0.101:5998 -manager-web 192.168.0.101:8000  -address :5000 -web-port :6001 > /dev/null 2>&1 &
 echo "Skywire node is now started and you can see it in on the Monitor." 
  
 
