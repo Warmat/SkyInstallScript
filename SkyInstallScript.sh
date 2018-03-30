643dc8b2471ceb1fd3931ba66702f42195c317b8
Warmat <31658555+Warmat@users.noreply.github.com>
2018-03-30 15:56:58 +0200

    Update SkyInstallScript.sh

--- SkyInstallScript.sh
+++ SkyInstallScript.sh
@@ -82,6 +82,7 @@ sudo mv go /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 
 ###### Reload the paths
+sleep 5
 . ~/.profile
 
 ###### Create GO directories
@@ -107,6 +108,7 @@ echo 'export GOPATH=$HOME/go' >> ~/.bashrc
 echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOBIN' >> ~/.bashrc
 ###### Reload GO paths
+sleep 5
 . ~/.bashrc
 echo "GO succesfully installed!"
  
